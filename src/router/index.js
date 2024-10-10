7f34bb7a3aa0334f85e014a58856259de8b201da
Razvan Zarnescu <zarnescurazvan98@gmail.com>
2024-10-10 16:46:40 +0300

    delete unused component

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,12 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
-import Products from '../views/ProductsList.vue';
-import ProductDetails from '../views/ProductDetails.vue';
-import Cart from '../views/ShoppingCart.vue';
+import ProductList from '../views/ProductsList.vue';
+import ShoppingCart from '../views/ShoppingCart.vue';
 
 const routes = [
     { path: '/', component: HomePage },
-    { path: '/products', component: Products },
-    { path: '/products/:id', component: ProductDetails, props: true },
-    { path: '/cart', component: Cart },
+    { path: '/products', component: ProductList },
+    { path: '/cart', component: ShoppingCart },
 ];
 
 const router = createRouter({

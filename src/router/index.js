48e69992438c5ece57d7d1264f3d4c21bc6adddf
Razvan Zarnescu <zarnescurazvan98@gmail.com>
2024-10-10 16:01:15 +0300

    Switch to hash mode for GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
 import Products from '../views/ProductsList.vue';
 import ProductDetails from '../views/ProductDetails.vue';
@@ -12,7 +12,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),  // Or use createWebHashHistory for local dev
+    history: createWebHashHistory(),
     routes,
 });
 
